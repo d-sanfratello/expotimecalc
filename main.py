041fe040abc13acb5dabcd7228676af30c9cc17d
da.sanfratello <da.sanfratello@gmail.com>
2021-02-26 18:57:58 +0100

    Added main.py and defined new method 'open_loc_file' into __init__.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import location as loc
+import time as time
+import skylocation as skyloc
+
+from . import open_loc_file as olf
+
+import sys
+
+
+def main(argv):
+    prompt = False
+
+    if len(argv) == 1:
+        log_geo = input("\tCoordinates of observatory:\t").upper()
+        obstime = input("\tTime of observation:\t").upper()
+        tgt = input("\tPosition of target:\t").upper()
+
+        prompt = True
+    else:
+        loc_geo, obstime, tgt = olf(argv[1], argv[2])
+
+    location = loc.Location(locstring=loc)
+    obsdate = time.Time(obstime)
+    target = skyloc.SkyLocation(locstring=tgt, obstime=obsdate)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
