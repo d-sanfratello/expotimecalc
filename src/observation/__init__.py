2dae90776308505a88a230967f1ac75b3832781e
da.sanfratello <da.sanfratello@gmail.com>
2021-02-27 17:29:50 +0100

    Added type-check for `Observation` constructor's attributes

--- src/observation/__init__.py
+++ src/observation/__init__.py
@@ -5,6 +5,13 @@ from .. import Versor
 
 class Observation:
     def __init__(self, location, obstime, target):
+        if not isinstance(location, Location):
+            raise TypeError("Must be of type `expotimecalc.location.Location`.")
+        if not isinstance(obstime, Time):
+            raise TypeError("Must be of type `expotimecalc.time.Time` or `astropy.time.Time`.")
+        if not isinstance(target, SkyLocation):
+            raise TypeError("Must be of type `expotimecalc.skylocation.SkyLocation`.")
+
         self.location = location
         self.obstime = obstime
         self.target = target
