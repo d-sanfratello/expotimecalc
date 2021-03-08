Converted `Tprec` to day units to have a comfortable conversiont to adimensional for operations. Corrected minor issue in `dms2dec` function (related to overwriting the built-in function `min`). Created GMSTeq2000 class to emulate a time object, that will eventually contain also the hour angle for Greenwich at the J2000 equinox. Added units to `Versor` class; redefined assigment of `ra` and `dec` based on units. While defining a `Versor` instance from a vector, the `Versor.vsr` attribute copies just the passed vector, before normalizing it.

src/__init__.py
@@ -5,9 +5,8 @@ from .time import Time
 
 
 Tsidday = 23.9345 * u.hour
-Tprec = 26000 * u.year
+Tprec = (26000 * u.year).to(u.day)
 tJ2000 = Time('J2000.0')
-# GMSTe2000 =
 
 
 def str2dms(string):
@@ -42,14 +41,14 @@ def dms2dec(dms):
         raise TypeError("`dms` must be a string.")
 
     deg, minsec = dms.lower().split('d')
-    min, sec = minsec.lower().split('m')
+    mins, sec = minsec.lower().split('m')
     sec = sec[:-1]
 
     deg = float(deg)
-    min = float(min) / 60
+    mins = float(mins) / 60
     sec = float(sec) / 3600
 
-    return deg + min + sec
+    return deg + mins + sec
 
 
 def open_loc_file(obs_path, tgt_path):
@@ -62,23 +61,54 @@ def open_loc_file(obs_path, tgt_path):
     return loc, obstime, tgt
 
 
+class GMSTeq2000:
+    """
+    ssd.jpl.nasa.gov/horizons.cgi
+    ephemType=OBSERVER
+    Target=Sun
+    Location=Geocentric
+    TimeSpan: 2000-03-20 07:25:00 - 2000-03-20 07:26:00, intervals=100
+    settings=default, display=default (HTML)
+    """
+    def __init__(self):
+        self.time = Time('2000-03-20T07:25:24.600', scale='ut1', format='isot')
+
+
+
+
 class Versor:
-    def __init__(self, ra=None, dec=None, vector=None):
-        if (ra is None and ra is None) and vector is None:
+    def __init__(self, ra=None, dec=None, vector=None, unit='deg'):
+        if unit not in ['deg', 'rad', 'hmsdms']:
+            raise ValueError("Must use a valid unit of measure.")
+
+        if (ra is None and dec is None) and vector is None:
             raise ValueError("Must give either a set of coordinates or a ra-dec position.")
 
         if ra is not None and dec is not None:
-            self.ra = ra
-            self.dec = dec
+            if unit == 'deg':
+                self.ra = ra
+                self.dec = dec
+            elif unit == 'rad':
+                self.ra = np.rad2deg(ra)
+                self.dec = np.rad2deg(dec)
+            else:
+                self.ra = hms2dms(ra)
+                self.dec = dms2dec(dec)
+
+            ra = np.deg2rad(self.ra)
+            dec = np.deg2rad(self.dec)
 
             self.vsr = np.array([np.cos(dec)*np.cos(ra),
                                  np.cos(dec)*np.sin(ra),
                                  np.sin(dec)], dtype=np.float64)
         else:
-            self.vsr = np.copy(vector/np.sqrt((vector**2).sum()))
+            self.vsr = np.copy(vector)/np.sqrt((vector**2).sum())
 
-            self.ra = np.arctan2(self.vsr[1]/self.vsr[0])
-            self.dec = np.arctan2(self.vsr[2]/np.sqrt(self.vsr[0]**2 + self.vsr[1]**2))
+            self.ra = np.arctan2(self.vsr[1], self.vsr[0])
+            self.dec = np.arctan2(self.vsr[2], np.sqrt(self.vsr[0]**2 + self.vsr[1]**2))
+
+            self.ra = np.rad2deg(self.ra)
+            self.dec = np.rad2deg(self.dec)
 
     def rotate(self, axis, angle, unit='rad', copy=False):
         r_mat = RotationMatrix(axis, angle, unit)
@@ -132,6 +162,7 @@ class RotationMatrix:
                              [             0,             0, 1]], dtype=np.float64)
         else:
             raise ValueError("Invalid axis")
+
 
 import src.location
 import src.observation