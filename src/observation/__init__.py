493553020c4450bc6763b3971feab2ec939dddd0
da.sanfratello <da.sanfratello@gmail.com>
2021-03-13 00:05:35 +0100

    Added `calculate_az`, `calculate_alt` and `plot_altaz_onday` methods. Modified `target_at`, `target_azimuth` to account for them. Modified `target.observe_at_date` method call to reflect the last commit on `src.observation`. Corrected bug in `zenithJ2000` where `eq2000` was called in a wrong way.

--- src/observation/__init__.py
+++ src/observation/__init__.py
@@ -1,14 +1,15 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 from astropy import units as u
 
-from ..location import Location
-from ..skylocation import SkyLocation
-from ..time import Time
-from .. import Versor
+from src.location import Location
+from src.skylocation import SkyLocation
+from src.time import Time
+from src import Versor
 
-from .. import Tsidday
-from .. import eq2000
+from src import Tsidday
+from src import eq2000
 
 
 class Observation:
@@ -24,20 +25,17 @@ class Observation:
         self.obstime = obstime
         self.target = target
 
-        self.target.observe_at_date(self.obstime)
+        self.target.observe_at_date(self.obstime, copy=False)
 
         self.zenithJ2000 = Versor(ra=0., dec=self.location.lat.rad, unit='rad')\
-            .rotate('z', self.sidereal_day(self.target.epoch) + eq2000.rad + self.location.lon.rad, unit='rad')
+            .rotate('z', self.sidereal_day(self.target.epoch) + eq2000.GMST.rad + self.location.lon, unit='rad')
 
         self.zenith = self.zenith_at_date(self.obstime)
 
         self.target_ha = self.calculate_ha(self.target, self.location, self.obstime)
-        self.target_azimuth = self.target_ha - 180 * u.deg
+        self.target_azimuth = self.calculate_az(self.target, self.location, self.obstime)
 
-        if ps := self.zenith.vsr.dot(self.target.vector_obstime.vsr) >= 0:
-            self.alt = np.rad2deg(np.arccos(ps)) * u.deg
-        else:
-            self.alt = -np.rad2deg(np.arccos(ps)) * u.deg
+        self.target_alt = self.calculate_alt()
 
     def zenith_at_date(self, obstime):
         return self.zenithJ2000.rotate('z', self.sidereal_day(obstime), unit='rad', copy=True)
@@ -50,3 +48,61 @@ class Observation:
 
     def calculate_ha(self, target, location, obstime):
         return self.LST(location, obstime).to(u.deg) - target.ra
+
+    def calculate_az(self, target, location, obstime):
+        return self.calculate_ha(target, location, obstime) - 180 * u.deg
+
+    def calculate_alt(self, zenith='default', target='default'):
+        if zenith != 'default' and not isinstance(zenith, Versor):
+            raise TypeError("Invalid zenith.")
+        if target != 'default' and not isinstance(target, Versor):
+            raise TypeError("Invalid target.")
+
+        if zenith == 'default':
+            zenith_vsr = self.zenith.vsr
+        else:
+            zenith_vsr = zenith.vsr
+
+        if target == 'default':
+            target_vsr = self.target.vector_obstime.vsr
+        else:
+            target_vsr = target.vsr
+
+        if ps := zenith_vsr.dot(target_vsr) >= 0:
+            return np.rad2deg(np.arccos(ps)) * u.deg
+        else:
+            return -np.rad2deg(np.arccos(ps)) * u.deg
+
+    def plot_altaz_onday(self, interval=15*u.min):
+        interval = interval.to(u.hour)
+        numpoints = int(24*u.hour/interval)
+
+        s_time = Time(int(self.obstime.mjd), format='mjd')
+        dt = 15*u.min * np.linspace(0, numpoints, num=numpoints + 1)
+        times = np.array([s_time + delta for delta in dt])
+
+        alt = np.empty(numpoints + 1, dtype=u.quantity.Quantity)
+        az = np.empty(numpoints + 1, dtype=u.quantity.Quantity)
+        index = 0
+
+        for t in times:
+            t_zenith = self.zenith_at_date(t)
+            tgt = self.target.observe_at_date(t, copy=True)
+
+            alt[index] = self.calculate_alt(t_zenith, tgt)
+            az[index] = self.calculate_az(tgt, self.location, t)
+
+            index += 1
+
+        plt.close(1)
+        plt.figure(1)
+
+        plt.subplot2grid(2,1,0,0)
+        plt.grid()
+        plt.plot(times, alt, 'k-')
+
+        plt.subplot2grid(2,1,1,0)
+        plt.grid()
+        plt.plot(times, az, 'k-')
+
+        plt.show()
