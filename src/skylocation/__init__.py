0bdbec7ff6b2bcda7fa1c69e8a91e52961477c54
da.sanfratello <da.sanfratello@gmail.com>
2021-03-13 16:14:13 +0100

    Name changes in base `src` are now reflected here. Added `__repr__` and `__str__` magic methods to show equatorial coordinates in hms-deg units. `nutation_corr` static method now yields an `astropy.quantity.Quantity` with an associated unit of measure (angle).

--- src/skylocation/__init__.py
+++ src/skylocation/__init__.py
@@ -1,25 +1,27 @@
 import numpy as np
 
-from astropy.time import Time
+from astropy import units as u
 
-from .. import location
-from .. import hms2dms
-from .. import dms2dec
-from .. import Versor
-from .. import Tprec
+from src.time import Time
+from src.location import Location
+from src import Versor
 
-from .. import tJ2000
+from src import hms2deg
+from src import dms2deg
+
+from src import Tprec
+from src import tJ2000
 
 
-class SkyLocation(location.Location):
-    def __init__(self, locstring=None, ra=None, dec=None, obstime=None, ra_unit='hms', dec_unit='deg', epoch='J2000'):
+class SkyLocation(Location):
+    def __init__(self, locstring=None, ra=None, dec=None, obstime=None, ra_unit='hour', dec_unit='deg', epoch='J2000'):
         if locstring is not None:
             ra, dec = locstring.split()
 
         if ra is not None and dec is not None:
             if isinstance(ra, str) and ra.lower().find("h") >= 0:
-                ra = hms2dms(ra)
-            elif ra_unit == 'hms':
+                ra = hms2deg(ra)
+            elif ra_unit == 'hour':
                 ra *= 15  # 1h = 15°
             elif ra_unit == 'deg':
                 ra = ra
@@ -29,7 +31,7 @@ class SkyLocation(location.Location):
                 raise ValueError("Unknown unit of measure in Right Ascension.")
 
             if isinstance(dec, str) and dec.lower().find("d") >= 0:
-                dec = dms2dec(dec)
+                dec = dms2deg(dec)
             elif dec_unit == 'deg':
                 dec = dec
             elif dec_unit == 'rad':
@@ -81,6 +83,25 @@ class SkyLocation(location.Location):
         else:
             self.vector_obstime = vector_obstime
 
+    def __str__(self):
+        ra, dec = self.__repr__().split()
+
+        if dec.find('-') >= 0:
+            return "RA:\t\t {0}\nDEC:\t{1}".format(ra, dec)
+        else:
+            return "RA:\t\t{0}\nDEC:\t{1}".format(ra, dec)
+
+    def __repr__(self):
+        ra = self.ra.hms
+        dec = np.array(self.dec.dms)
+        if dec[0] < 0:
+            dec[1] *= -1
+            dec[2] *= -1
+
+        ra = "{0:d}h{1:d}m{2:.3f}s".format(int(ra[0]), int(ra[1]), ra[2])
+        dec = "{0:d}d{1:d}m{2:.3f}s".format(int(dec[0]), int(dec[1]), dec[2])
+        return ra + " " + dec
+
     @staticmethod
     def equinox_prec_corr(obstime):
         return (2*np.pi/Tprec.value) * (obstime - tJ2000).jd
@@ -88,4 +109,4 @@ class SkyLocation(location.Location):
     @staticmethod
     def nutation_corr(obstime):
         # check for Earth Fact Sheet at https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
-        return np.deg2rad(23.44)
+        return 23.44 * u.deg
