8a658f1d31b6ed36212a6c2eef936e6535320ca1
da.sanfratello <da.sanfratello@gmail.com>
2021-03-20 12:42:17 +0100

    Created `src.warnmsg` module, containing all warning messages to be displayed.

---
+++ src/warnmsg.py
@@ -0,0 +1,2 @@
+airmassWarning = "\nAirmass is calculated for a uniform, plane-parallel atmosphere. Hence, airmass value is just an upper limit at lower and lower altitudes above the horizon."
+visibilityWarning = "\nAt the moment `calculate_visibility` method does not take into account the Sun's position."
