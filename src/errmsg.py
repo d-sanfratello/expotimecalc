8c9afd026a60b2e20c0ddd149a45d6c29fed26ef
da.sanfratello <da.sanfratello@gmail.com>
2021-03-20 12:43:55 +0100

    Created `src.errmsg` module, containing all error messages to be displayed.

---
+++ src/errmsg.py
@@ -0,0 +1,23 @@
+# Epoch errors
+invalidEpoch = "`epoch` is not a valid epoch."
+
+# Type errors
+notTwoTypesError = "`{}` must be either `{}` or `{}`."
+notTypeError = "`{}` must be of type {}."
+latLonWrongTypeError = "`lat` or `lon` attributes are of wrong type."
+latNotLonError = "`lat` cannot be of type `Longitude`."
+lonNotLatError = "`lon` cannot be of type `Latitude`."
+
+# Not Implemented errors
+epochNotImplemented = "This feature has not been implemented yet, as other epochs are not available."
+
+# Units errors
+invalidUnitError = "Invalid unit of measure."
+specifyUnitError = "Must specify the unit of measure."
+
+# Value errors
+invalidAxisError = "Not a valid rotation axis."
+mustDeclareLatLonError = "Must declare both latitude and longitude."
+mustDeclareLocation = "Must declare location of observatory."
+mustDeclareRaDecError = "Must position of sky object."
+versorError = "Must give either a set of coordinates or a ra-dec position."
