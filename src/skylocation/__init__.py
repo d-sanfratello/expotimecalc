b6108c704b2f3c82db617bbba71357bfe733ef67
da.sanfratello <da.sanfratello@gmail.com>
2021-03-22 11:44:41 +0100

    Updated `__str__` method to display both epoch and date position. Updated `__repr__` method to display date position and not epoch position.

--- src/skylocation/__init__.py
+++ src/skylocation/__init__.py
@@ -65,8 +65,10 @@ class SkyLocation(Location):
 
         super(SkyLocation, self).__init__(locstring, lat=dec, lon=ra, is_sky=True)
 
-        self.dec = self.__dict__.pop('lat')
-        self.ra = self.__dict__.pop('lon')
+        self.dec_epoch = self.__dict__.pop('lat')
+        self.ra_epoch = self.__dict__.pop('lon')
+        self.ra = None
+        self.dec = None
 
         self.name = self.name_object(name, epoch)
 
@@ -76,7 +78,7 @@ class SkyLocation(Location):
             self.obstime = obstime
         self.epoch = Time(epoch)
 
-        self.vector_epoch = Versor(self.ra, self.dec)
+        self.vector_epoch = Versor(self.ra_epoch, self.dec_epoch)
         self.vector_obstime = None
         self.at_date(self.obstime)
 
@@ -91,8 +93,8 @@ class SkyLocation(Location):
             .rotate_inv('z', self.equinox_prec(self.obstime), copy=True)\
             .rotate('x', self.axial_tilt(self.obstime), copy=True)
 
-        self.ra = self.vector_epoch.ra
-        self.dec = self.vector_epoch.dec
+        self.ra_epoch = self.vector_epoch.ra
+        self.dec_epoch = self.vector_epoch.dec
 
     def precession_at_date(self, obstime):
         if not isinstance(obstime, Time):
@@ -110,6 +112,8 @@ class SkyLocation(Location):
 
         self.obstime = obstime
         self.vector_obstime = self.precession_at_date(obstime)
+        self.ra = self.vector_obstime.ra
+        self.dec = self.vector_obstime.dec
 
     def name_object(self, name, epoch):
         if name is None:
@@ -123,12 +127,30 @@ class SkyLocation(Location):
             return name
 
     def __str__(self):
+        ra_epoch = self.ra_epoch.hms
+        dec_epoch = np.array(self.dec_epoch.dms)
         ra, dec = self.__repr__().split()
 
-        if dec.find('-') >= 0:
-            return "RA:\t\t {0}\nDEC:\t{1}".format(ra, dec)
+        if dec_epoch[0] < 0:
+            dec_epoch[1] *= -1
+            dec_epoch[2] *= -1
+
+        ra_epoch = "{0:d}h{1:d}m{2:.3f}s".format(int(ra_epoch[0]), int(ra_epoch[1]), ra_epoch[2])
+        dec_epoch = "{0:d}d{1:d}m{2:.3f}s".format(int(dec_epoch[0]), int(dec_epoch[1]), dec_epoch[2])
+
+        string = "\t{} (Epoch):\n".format(Equinox2000.time.iso)
+        if dec_epoch.find('-') >= 0:
+            string_epoch = "RA:\t\t {0}\nDEC:\t{1}\n\n".format(ra_epoch, dec_epoch)
         else:
-            return "RA:\t\t{0}\nDEC:\t{1}".format(ra, dec)
+            string_epoch = "RA:\t\t{0}\nDEC:\t{1}\n\n".format(ra_epoch, dec_epoch)
+
+        string += string_epoch + "\t{} (Date):\n".format(self.obstime.iso)
+        if dec.find('-') >= 0:
+            string += "RA:\t\t {0}\nDEC:\t{1}".format(ra, dec)
+        else:
+            string += "RA:\t\t{0}\nDEC:\t{1}".format(ra, dec)
+
+        return string
 
     def __repr__(self):
         ra = self.ra.hms
