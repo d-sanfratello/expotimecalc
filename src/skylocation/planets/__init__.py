9b6fdb25d4052de317db88eb5ad984a3e8c55204
da.sanfratello <da.sanfratello@gmail.com>
2021-05-29 13:39:38 +0200

    First attempt at working planets. -   Now `at_date` and `observe_at_date` directly check for JPL ephemeris to     determine osculating elements at observation time. -   Every call at osculating elements within the given planets (Earth and Venus)     has been removed. -   `argument_perihelion` has been renamed to `argument_pericenter`, to be able     to use the same mechanics for, potentially, other objects.

--- src/skylocation/planets/__init__.py
+++ src/skylocation/planets/__init__.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pykep as pk
+# https://ui.adsabs.harvard.edu/abs/2015arXiv151100821I/abstract
 
 from astropy import units as u
 from astropy import constants as cts
@@ -20,23 +22,10 @@ from .. import errmsg
 class Planet(SkyLocation):
     __private_arguments = ['is_earth']
 
-    def __init__(self, obstime, *, semimaj, eccentricity, longitude_an, inclination,
-                 argument_perihelion, mean_anomaly,
-                 name=None, epoch='J2000', **kwargs):
+    def __init__(self, obstime, *, name=None, epoch='J2000', **kwargs):
 
         if not isinstance(obstime, Time):
             raise TypeError(errmsg.notTwoTypesError.format('obstime', 'src.time.Time', 'astropy.time.Time'))
-        if not isinstance(semimaj, u.Quantity):
-            raise TypeError(errmsg.notTypeError('semimaj', 'astropy.units.Quantity'))
-        if not isinstance(eccentricity, Number):
-            raise TypeError(errmsg.notTypeError('eccentricity', 'Number'))
-        if not isinstance(longitude_an, (Angle, Longitude)):
-            raise TypeError(errmsg.notTwoTypesError.format('longitude_an', 'astropy.coordinates.Angle',
-                                                           'astropy.coordinates.angles.Longitude'))
-        if not isinstance(inclination, Angle):
-            raise TypeError(errmsg.notTypeError('orbit_inclination', 'astropy.coordinates.Angle'))
-        if not isinstance(argument_perihelion, Angle):
-            raise TypeError(errmsg.notTypeError('argument_periapsis', 'astropy.coordinates.Angle'))
         if name is None:
             raise ValueError(errmsg.mustDeclareName)
 
@@ -53,54 +42,53 @@ class Planet(SkyLocation):
         self.__mass_sun = cts.M_sun
         self.__grav_const = cts.G * self.__mass_sun / (4 * np.pi ** 2)
 
-        self.__semimaj = semimaj
-        self.__ecc = eccentricity
-        self.__argument_perihelion = argument_perihelion
-        self.__mean_anomaly = mean_anomaly
-        self.__inclination = inclination
-        self.__longitude_an = longitude_an
-
-        self.__initialize()
-
-        super(Planet, self).__init__(locstring=None, ra=self.vector_epoch.ra, dec=self.vector_epoch.dec,
-                                     distance=self.distance_from_sun, obstime=obstime,
-                                     ra_unit='deg', dec_unit='deg', epoch=epoch, name=name)
+        self.__semimaj = None
+        self.__ecc = None
+        self.__argument_pericenter = None
+        self.__mean_anomaly = None
+        self.__inclination = None
+        self.__longitude_an = None
 
         self.at_date(obstime)
+
+        super(Planet, self).__init__(locstring=None, ra=self.vector_obstime.ra, dec=self.vector_obstime.dec,
+                                     distance=self.distance_from_sun, obstime=obstime,
+                                     ra_unit='deg', dec_unit='deg', epoch=epoch, name=name)
 
     def observe_at_date(self, obstime):
         if not isinstance(obstime, Time):
             raise TypeError(errmsg.notTwoTypesError.format('obstime', 'src.time.Time', 'astropy.time.Time'))
 
-        # Il pianeta, che è stato inizializzato alla sua posizione iniziale all'epoca J2000, ha il versore ruotato
-        # per avere il nodo ascendente dell'orbita coincidente con l'asse 'x', intorno a cui l'orbita viene inclinata
-        # rispetto al piano dell'eclittica. Dopo l'applicazione del moto di rivoluzione intorno al Sole, si riporta
-        # il pianeta sul piano della sua orbita, rispetto all'eclittica. Quindi si sottrae il moto della Terra,
-        # intorno al Sole. Al vettore ottenuto, si applica la precessione degli equinozi per la correzione delle
-        # coordinate equatoriali alla data e si trasforma il vettore in coordinate equatoriali, applicando una
-        # rotazione intorno all'asse 'x'.
-        # vector_obstime = (self.heliocentric_planet_J2000.rotate_inv('z', angle=self.longitude_an, copy=True)
-        #                       .rotate_inv('x', angle=self.orbit_inclination, copy=True)
-        #                       .rotate('z', angle=self.revolution(obstime), copy=True)
-        #                       .rotate('x', angle=self.orbit_inclination, copy=True)
-        #                       .rotate('z', angle=self.longitude_an, copy=True)
-        #                   - self.heliocentric_earth_J2000.rotate('z', angle=self.year_revolution(obstime), copy=True)) \
-        #     .rotate('z', angle=self.equinox_prec(obstime), copy=True) \
-        #     .rotate('x', angle=self.axial_tilt(obstime), copy=True)
+        epoch = pk.epoch_from_string(obstime.iso)
+
+        __semimaj, __ecc, __inclination, __ra_an, __peri_arg, __mean_anomaly = self.ephemeris.osculating_elements(epoch)
+        __semimaj *= u.m
+        self.__semimaj = __semimaj.to(cts.au)
+
+        __inclination *= u.rad
+        self.__inclination = Angle(__inclination.to(u.deg))
+
+        __ra_an *= u.rad
+        self.__longitude_an = Longitude(__ra_an.to(u.deg))
+
+        __peri_arg *= u.rad
+        self.__argument_pericenter = Angle(__peri_arg.to(u.deg))
+        self.__ecc = __ecc
+        self.__mean_anomaly = __mean_anomaly
+
         if self.__is_earth:
             reference_observer = Versor(vector=np.zeros(3) * cts.au)
         else:
             from .earth import Earth
             reference_observer = Earth(obstime=obstime).vector_obstime
 
-        heliocentric = Versor(ra=0 * u.deg, dec=0 * u.deg, radius=self.distance_from_sun) \
-            .rotate(axis='z', angle=self.revolution(obstime, reference=tJ2000), copy=True) \
+        vector_obstime = Versor(vector=np.array([1, 0, 0]) * self.distance_from_sun) \
             .rotate(axis='z', angle=self.mean_anomaly, copy=True) \
-            .rotate(axis='z', angle=self.argument_perihelion, copy=True) \
+            .rotate(axis='z', angle=self.argument_pericenter, copy=True) \
             .rotate(axis='x', angle=self.inclination, copy=True) \
             .rotate(axis='z', angle=self.longitude_an, copy=True)
 
-        vector_obstime = (heliocentric - reference_observer) \
+        vector_obstime = (vector_obstime - reference_observer) \
             .rotate(axis='x', angle=self.axial_tilt(obstime), copy=True)  # check for rotate or rotate_inv
 
         return vector_obstime
@@ -113,48 +101,6 @@ class Planet(SkyLocation):
         self.vector_obstime = self.observe_at_date(obstime)
         self.ra = self.vector_obstime.ra
         self.dec = self.vector_obstime.dec
-
-    def revolution(self, obstime, epoch_eq='equinoxJ2000', reference=None):
-        if not isinstance(obstime, Time):
-            raise TypeError(errmsg.notTwoTypesError.format('obstime', 'src.time.Time', 'astropy.time.Time'))
-        if epoch_eq != 'equinoxJ2000':
-            raise NotImplementedError(errmsg.epochNotImplemented)
-        if reference is not None and not isinstance(reference, Time):
-            raise TypeError(errmsg.notTypeError.format('reference', 'astropy.time.Time'))
-
-        if reference is None:
-            reference = self.equinoxes[epoch_eq]
-            ref_time = reference.time
-        else:
-            ref_time = reference
-
-        # Restituisce la fase accumulata, in radianti, in rapporto al periodo di rivoluzione del pianeta.
-        return ((2 * np.pi / self.revolution_period.value) * (obstime - ref_time).jd) % (2 * np.pi) * u.rad
-
-    def __initialize(self):
-        if self.__is_earth:
-            reference_observer = Versor(vector=np.zeros(3) * cts.au)
-        else:
-            from .earth import Earth
-            reference_observer = Earth(obstime=Equinox2000.time).vector_obstime
-
-        self.helioc_eq2000 = Versor(ra=0 * u.deg, dec=0 * u.deg, radius=self.distance_from_sun) \
-            .rotate(axis='z', angle=self.revolution(Equinox2000.time, reference=tJ2000), copy=True) \
-            .rotate(axis='z', angle=self.mean_anomaly, copy=True) \
-            .rotate(axis='z', angle=self.argument_perihelion, copy=True) \
-            .rotate(axis='x', angle=self.inclination, copy=True) \
-            .rotate(axis='z', angle=self.longitude_an, copy=True)
-
-        self.vector_epoch = (self.helioc_eq2000 - reference_observer) \
-            .rotate(axis='x', angle=self.axial_tilt(Equinox2000.time), copy=True)  # check for rotate or rotate_inv
-
-    # # noinspection PyPep8Naming
-    # @property
-    # def heliocentric_planet_J2000(self):
-    #     helioc_planet = self.vector_epoch.rotate_inv('z', angle=self.axial_tilt(Equinox2000.time), copy=True)\
-    #                     + self.heliocentric_earth_J2000
-    #
-    #     return helioc_planet
 
     @property
     def longitude_an(self):
@@ -172,13 +118,14 @@ class Planet(SkyLocation):
     def eccentricity(self):
         return self.__ecc
 
+    # noinspection PyTypeChecker
     @property
     def semimin(self):
         return self.semimaj * np.sqrt(1 - self.eccentricity**2)
 
     @property
-    def argument_perihelion(self):
-        return self.__argument_perihelion
+    def argument_pericenter(self):
+        return self.__argument_pericenter
 
     @property
     def mean_anomaly(self):
@@ -188,10 +135,12 @@ class Planet(SkyLocation):
     def inclination(self):
         return self.__inclination
 
+    # noinspection PyTypeChecker
     @property
     def peri_dist(self):
         return self.semimaj * (1 - self.eccentricity)
 
+    # noinspection PyTypeChecker
     @property
     def apo_dist(self):
         return self.semimaj * (1 + self.eccentricity)
--- src/skylocation/planets/earth.py
+++ src/skylocation/planets/earth.py
@@ -1,12 +1,6 @@
 import pykep as pk
 # https://ui.adsabs.harvard.edu/abs/2015arXiv151100821I/abstract
 
-from astropy import units as u
-from astropy import constants as cts
-from astropy.coordinates import Angle
-from astropy.coordinates import Longitude
-
-from ... import Equinox2000
 from ...time import Time
 from . import Planet
 
@@ -19,21 +13,5 @@ class Earth(Planet):
             raise TypeError(errmsg.notTwoTypesError.format('obstime', 'src.time.Time', 'astropy.time.Time'))
 
         self.ephemeris = pk.planet.jpl_lp('earth')
-        epoch = pk.epoch_from_string(Equinox2000.time.iso)
 
-        __semimaj, __ecc, __inclination, __ra_an, __peri_arg, __mean_anomaly = self.ephemeris.osculating_elements(epoch)
-        __semimaj *= u.m
-        __semimaj = __semimaj.to(cts.au)
-
-        __inclination *= u.rad
-        __inclination = Angle(__inclination.to(u.deg))
-
-        __ra_an *= u.rad
-        __longitude_an = Longitude(__ra_an.to(u.deg))
-
-        __peri_arg *= u.rad
-        __peri_arg = Angle(__peri_arg.to(u.deg))
-
-        super(Earth, self).__init__(obstime, semimaj=__semimaj, eccentricity=__ecc, longitude_an=__longitude_an,
-                                    inclination=__inclination, argument_perihelion=__peri_arg,
-                                    mean_anomaly=__mean_anomaly, name='Earth', epoch=obstime.iso, is_earth=True)
+        super(Earth, self).__init__(obstime, name='Earth', epoch=obstime.iso, is_earth=True)
